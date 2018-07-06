6f55dfd00e264d21df515ae3c1a406293e72d00d
Aisha Coles <coles_aisha@yahoo.com>
2018-07-05 21:40:37 -0700

    loop working color needed

--- js/designs.js
+++ js/designs.js
@@ -7,25 +7,28 @@ sizePicker.addEventListener('submit', function(event) {
 	event.preventDefault();
   	height = document.getElementById('inputHeight').value;
 	width = document.getElementById('inputWidth').value;
+	color = document.getElementById('colorPicker').value;
 	makeGrid(height, width);
 });
 
 
-var rows = document.createElement('tr');
-var cols = document.createElement('td');
+var rows;
+var cols;
 function makeGrid (height, width) {
-	for (var i =1; i <= height; i++) {
-		document.querySelector('#pixelCanvas').appendChild(rows);
-		for( var j = 1; j <= width; i++) {
+	for (var i = 1; i <= height; i++) {
+		rows = document.createElement('tr');
+		document.getElementById('pixelCanvas').appendChild(rows);
+		for( var j = 1; j <= width; j++) {
+		cols = document.createElement('td');
 		rows.appendChild(cols);
 		}
 	}
-	color = document.getElementById('colorPicker').value;
+	//color = document.getElementById('colorPicker').value;
 	cols.addEventListener('click', function addColor() {
-	if($(this).attr('style')) {
-			$(this).removeAttr('style')
+	if(cols.setAttribute('style','')) {
+		cols.style.backgroundColor = color;
 		} else {
-			$(this).attr('style', 'background-color:' + color);
+			cols.style.removeAttribute('style');
 		}	
 	
 });
